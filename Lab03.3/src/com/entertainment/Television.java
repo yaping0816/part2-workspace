1fcb78df283a775f095d73e52e425264fde338f4
yaping0816 <yaping0816@gmail.com>
2022-09-21 19:10:13 -0700

    Completed lab 3.3

--- Lab03.3/src/com/entertainment/Television.java
+++ Lab03.3/src/com/entertainment/Television.java
@@ -11,93 +11,92 @@ package com.entertainment;
 import java.util.Objects;
 
 public class Television
-implements Comparable<Television> {
+    implements Comparable<Television> {
+
   public static final int MIN_VOLUME = 0;
   public static final int MAX_VOLUME = 100;
   public static final int MIN_CHANNEL = 1;
   public static final int MAX_CHANNEL = 999;
-  
+
   private String brand;
   private int volume;
   private DisplayType display;
   private Tuner tuner = new Tuner();  // set up internally and used for channel management
-  
+
   public Television() {
   }
-  
+
   public Television(String brand) {
     setBrand(brand);
   }
-  
+
   public Television(String brand, int volume)
-  throws IllegalArgumentException {
+      throws IllegalArgumentException {
     this(brand);
     setVolume(volume);
   }
-  
+
   public Television(String brand, DisplayType display) {
     this(brand);
     setDisplay(display);
   }
-  
+
   public Television(String brand, int volume, DisplayType display)
-  throws IllegalArgumentException {
+      throws IllegalArgumentException {
     this(brand, volume);
     setDisplay(display);
   }
-  
+
   public String getBrand() {
     return this.brand;
   }
-  
+
   public void setBrand(String brand) {
     this.brand = brand;
   }
-  
+
   public int getVolume() {
     return this.volume;
   }
-  
+
   public void setVolume(int volume)
-  throws IllegalArgumentException {
+      throws IllegalArgumentException {
     if (volume >= MIN_VOLUME && volume <= MAX_VOLUME) {
       this.volume = volume;
-    }
-    else {
-      throw new IllegalArgumentException("Invalid volume: " + volume + ". " + 
-        "Allowed range: [" + MIN_VOLUME + "," + MAX_VOLUME + "].");
+    } else {
+      throw new IllegalArgumentException("Invalid volume: " + volume + ". " +
+          "Allowed range: [" + MIN_VOLUME + "," + MAX_VOLUME + "].");
     }
   }
-  
+
   public int getCurrentChannel() {
     return tuner.getChannel();  // delegate to contained Tuner object
   }
-  
+
   public void changeChannel(int channel)
-  throws InvalidChannelException {
+      throws InvalidChannelException {
     if (channel >= MIN_CHANNEL && channel <= MAX_CHANNEL) {
       tuner.setChannel(channel);  // delegate to contained Tuner object
-    }
-    else {
+    } else {
       throw new InvalidChannelException("Invalid channel: " + channel + ". " +
-        "Allowed range: [" + MIN_CHANNEL + "," + MAX_CHANNEL + "].");
+          "Allowed range: [" + MIN_CHANNEL + "," + MAX_CHANNEL + "].");
     }
   }
-  
+
   public DisplayType getDisplay() {
     return this.display;
   }
-  
+
   public void setDisplay(DisplayType display) {
     this.display = display;
   }
 
   @Override
-  public String toString() { 
-    return getClass().getSimpleName() + " [brand=" + getBrand() + ", volume=" + getVolume() + 
-      ", currentChannel=" + getCurrentChannel() + ", display=" + getDisplay() + "]";
+  public String toString() {
+    return getClass().getSimpleName() + " [brand=" + getBrand() + ", volume=" + getVolume() +
+        ", currentChannel=" + getCurrentChannel() + ", display=" + getDisplay() + "]";
   }
-  
+
   @Override
   public int compareTo(Television other) {
     return this.getBrand().compareTo(other.getBrand());
@@ -113,9 +112,9 @@ implements Comparable<Television> {
     boolean result = false;
     if (obj instanceof Television) {
       Television other = (Television) obj;
-      result = Objects.equals(this.getBrand(),   other.getBrand()) &&
-               Objects.equals(this.getVolume(),  other.getVolume()) &&
-               Objects.equals(this.getDisplay(), other.getDisplay());
+      result = Objects.equals(this.getBrand(), other.getBrand()) &&
+          Objects.equals(this.getVolume(), other.getVolume()) &&
+          Objects.equals(this.getDisplay(), other.getDisplay());
     }
     return result;
   }
--- Lab03.3/test/com/entertainment/TelevisionTest.java
+++ Lab03.3/test/com/entertainment/TelevisionTest.java
@@ -22,29 +22,62 @@ public class TelevisionTest {
 
   @Test
   public void setVolumeIllegalArgumentsException() {
+    int oldVolume = tv.getVolume();
     try{
       tv.setVolume(-20);
       fail("Should have thrown exceptions");
     }catch(IllegalArgumentException e){
-      assertEquals("Invalid volume: -20. Allowed range: [0,100].", e.getMessage());
+      assertEquals(oldVolume, tv.getVolume());
+    }
+  }
+
+  @Test
+  public void changeChannel() throws InvalidChannelException {
+    int channel = (Television.MAX_CHANNEL+Television.MAX_CHANNEL)/2;
+    tv.changeChannel(channel);
+    assertEquals(channel, tv.getCurrentChannel());
+  }
+
+  @Test
+  public void testChangeChannelInvalidChannelException() throws InvalidChannelException{
+    int oldChannel = tv.getCurrentChannel();
+    try{
+      tv.changeChannel(1000);
+    }catch(InvalidChannelException e){
+      assertEquals(oldChannel, tv.getCurrentChannel());
     }
 
   }
 
   @Test
-  public void changeChannel() throws InvalidChannelException {
-    tv.changeChannel(300);
-  }
-
-  @Test
   public void compareTo() {
+    Television other1 = new Television();
+    other1.setBrand("LG");
+
+    Television other2 = new Television();
+    other2.setBrand("Samsung");
+
+    assertEquals(0, tv.compareTo(other1));
+    assertTrue(tv.compareTo(other2)<0);
+
   }
 
   @Test
   public void testHashCode() {
+    Television tv1 = new Television("LG", 25,DisplayType.LED);
+    Television tv2 = new Television("Samsung", 50, DisplayType.PLASMA);
+
+    assertEquals(tv.hashCode(), tv1.hashCode());
+    assertNotEquals(tv.hashCode(), tv2.hashCode());
+
   }
 
   @Test
   public void testEquals() {
+    Television tv1 = new Television("LG", 25,DisplayType.LED);
+    Television tv2 = new Television("Samsung", 50, DisplayType.PLASMA);
+
+    assertEquals(tv, tv1);
+    assertNotEquals(tv,tv2);
   }
 }
